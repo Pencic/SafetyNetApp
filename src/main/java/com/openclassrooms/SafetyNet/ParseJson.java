cfa19c08e30fd09d8f5512c40c2cfc6a0fce20b5
Nikola Pencic <pencic993@gmail.com>
2020-02-22 00:16:00 +0100

    Update ParseJson.java

--- src/main/java/com/openclassrooms/SafetyNet/ParseJson.java
+++ src/main/java/com/openclassrooms/SafetyNet/ParseJson.java
@@ -1,5 +1,25 @@
 package com.openclassrooms.SafetyNet;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class ParseJson {
+	
+	  @SuppressWarnings("unchecked")
+	    public static void Parse()   {
+	        
+	       try  {
+	    	         	   
+	    	   CompositeModel Model = new ObjectMapper().readValue(new File("src/main/resources/data.json"), CompositeModel.class );
+	    	      	         
+	       } catch (FileNotFoundException e) {
+	        e.printStackTrace();
+	       } catch (IOException e) {
+	        e.printStackTrace();
+	       }                       
+	    }
 
 }
