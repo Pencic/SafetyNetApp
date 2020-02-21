27fefc2c37d6cb04b0275f0d46ab8e7fd9815202
Nikola Pencic <pencic993@gmail.com>
2020-02-22 00:15:58 +0100

    Update MedicalrecordsModel.java

--- src/main/java/com/openclassrooms/SafetyNet/MedicalrecordsModel.java
+++ src/main/java/com/openclassrooms/SafetyNet/MedicalrecordsModel.java
@@ -1,5 +1,49 @@
 package com.openclassrooms.SafetyNet;
 
+import java.util.Set;
+
 public class MedicalrecordsModel {
+	
+	private String firstName;
+	private String lastName;
+	private String birthdate;
+	private Set<String> medications;
+	private Set<String> allergies;
+	
+	public MedicalrecordsModel() {
+		
+	}
+	
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getBirthdate() {
+		return birthdate;
+	}
+	public void setBirthdate(String birthdate) {
+		this.birthdate = birthdate;
+	}
+	public Set<String> getMedications() {
+		return medications;
+	}
+	public void setMedications(Set<String> medications) {
+		this.medications = medications;
+	}
+	public Set<String> getAllergies() {
+		return allergies;
+	}
+	public void setAllergies(Set<String> allergies) {
+		this.allergies = allergies;
+	}
 
 }
