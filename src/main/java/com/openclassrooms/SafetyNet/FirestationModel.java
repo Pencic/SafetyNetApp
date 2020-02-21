cffb2b5d349ebe33676d285ccc742fb60ddfb597
Nikola Pencic <pencic993@gmail.com>
2020-02-22 00:15:55 +0100

    Update FirestationModel.java

--- src/main/java/com/openclassrooms/SafetyNet/FirestationModel.java
+++ src/main/java/com/openclassrooms/SafetyNet/FirestationModel.java
@@ -1,5 +1,28 @@
 package com.openclassrooms.SafetyNet;
 
 public class FirestationModel {
+	
+	private String address;
+	private int station;
+	
+	public FirestationModel() {
+		
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String adress) {
+		this.address = adress;
+	}
+
+	public int getStation() {
+		return station;
+	}
+
+	public void setStation(int station) {
+		this.station = station;
+	}
 
 }
