f191341a0440f72aa5595ad37a80d029b7cd26cd
Nikola Pencic <pencic993@gmail.com>
2020-02-22 00:15:26 +0100

    Method for firestation zones

--- src/main/java/com/openclassrooms/SafetyNet/CompositeModel.java
+++ src/main/java/com/openclassrooms/SafetyNet/CompositeModel.java
@@ -1,5 +1,57 @@
 package com.openclassrooms.SafetyNet;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CompositeModel {
+	
+	private List<PersonModel> persons;
+	private List<FirestationModel> firestations;
+	private List<MedicalrecordsModel> medicalrecords;
+	
+	public CompositeModel() {
+		
+	}
+	
+	public List<String> getAdressesForStation(int station) {
+		
+		if(getFirestations() == null)
+			return null;
+		List<String> result = new ArrayList<String>();
+		
+		FirestationModel[] firestationModels = new FirestationModel[getFirestations().size()];
+		
+		for(int i = 0 ; i < getFirestations().size(); i ++) {
+			FirestationModel model = getFirestations().toArray(firestationModels)[i];
+			if (model.getStation() == station) {
+				result.add(model.getAddress());
+		    }
+	    }
+		return result;		
+	}
+
+	public List<PersonModel> getPersons() {
+		return persons;
+	}
+
+	public void setPersons(List<PersonModel> persons) {
+		this.persons = persons;
+	}
+
+	public List<FirestationModel> getFirestations() {
+		return firestations;
+	}
+
+	public void setFirestations(List<FirestationModel> firestations) {
+		this.firestations = firestations;
+	}
+
+	public List<MedicalrecordsModel> getMedicalrecords() {
+		return medicalrecords;
+	}
+
+	public void setMedicalrecords(List<MedicalrecordsModel> medicalrecords) {
+		this.medicalrecords = medicalrecords;
+	}
 
 }
