0f8ca78e4036b000e2ce2d025a8f8f7b46d684d0
Nikola Pencic <pencic993@gmail.com>
2020-02-21 17:08:08 +0100

    ParseJson

--- src/main/java/com/openclassrooms/SafetyNet/SafetyNetApplication.java
+++ src/main/java/com/openclassrooms/SafetyNet/SafetyNetApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SafetyNetApplication {
 
 	public static void main(String[] args) {
+		ParseJson.Parse();
 		SpringApplication.run(SafetyNetApplication.class, args);
 	}
 
